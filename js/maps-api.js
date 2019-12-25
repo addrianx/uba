b7e627d9f0b9b8e39767acf16b2205c9fb988ff4
adipatria <adipatrianugrah94a@gmail.com>
2019-12-25 16:53:53 +0700

    commit terbaru

--- js/maps-api.js
+++ js/maps-api.js
@@ -1,37 +1,81 @@
-function addMarkersToMap(map) {
-   var uba_office = new H.map.Marker({lat:-7.231817, lng:107.899836});
-   map.addObject(uba_office);
+function addDraggableMarker(map, behavior) {
+
+  var marker = new H.map.Marker({ lat: -7.231817, lng: 107.899836 }, {
+    // mark the object as volatile for the smooth dragging
+    volatility: true
+  });
+  // Ensure that the marker can receive drag events
+  marker.draggable = true;
+  map.addObject(marker);
+
+  // disable the default draggability of the underlying map
+  // and calculate the offset between mouse and target's position
+  // when starting to drag a marker object:
+  map.addEventListener('dragstart', function (ev) {
+    var target = ev.target,
+      pointer = ev.currentPointer;
+    if (target instanceof H.map.Marker) {
+      var targetPosition = map.geoToScreen(target.getGeometry());
+      target['offset'] = new H.math.Point(pointer.viewportX - targetPosition.x, pointer.viewportY - targetPosition.y);
+      behavior.disable();
+    }
+  }, false);
+
+
+  // re-enable the default draggability of the underlying map
+  // when dragging has completed
+  map.addEventListener('dragend', function (ev) {
+    var target = ev.target;
+    if (target instanceof H.map.Marker) {
+      behavior.enable();
+    }
+  }, false);
+
+  // Listen to the drag event and move the position of the marker
+  // as necessary
+  map.addEventListener('drag', function (ev) {
+    var target = ev.target,
+      pointer = ev.currentPointer;
+    if (target instanceof H.map.Marker) {
+      target.setGeometry(map.screenToGeo(pointer.viewportX - target['offset'].x, pointer.viewportY - target['offset'].y));
+    }
+  }, false);
 }
 
-  //Initialize the Platform object:
-  var platform = new H.service.Platform({
-    'apikey': 'bya_2amiUMMXCIxyhDjOWeC4surSQTV2KUUmaU6k6g4'
-  });
+/**
+ * Boilerplate map initialization code starts below:
+ */
 
-  // Get the default map types from the Platform object:
-  var defaultLayers = platform.createDefaultLayers();
+//Step 1: initialize communication with the platform
+// In your own code, replace variable window.apikey with your own apikey
+var platform = new H.service.Platform({
+  apikey: 'bya_2amiUMMXCIxyhDjOWeC4surSQTV2KUUmaU6k6g4'
+});
+var defaultLayers = platform.createDefaultLayers();
 
-  // Instantiate the map:
-  var map = new H.Map(
-    document.getElementById('mapContainer'),
-    defaultLayers.vector.normal.map,
-    {
-      zoom: 16,
-      center: { lng: 107.899836, lat: -7.231817 }
-    });
+//Step 2: initialize a map - this map is centered over Boston
+var map = new H.Map(document.getElementById('mapContainer'),
+  defaultLayers.vector.normal.map, {
+  center: { lng: 107.899836, lat: -7.231817 },
+  zoom: 16,
+  pixelRatio: window.devicePixelRatio || 1
+});
+// add a resize listener to make sure that the map occupies the whole container
+window.addEventListener('resize', () => map.getViewPort().resize());
 
-    window.onload = function () {
-    addMarkersToMap(map);
-    } 
+//Step 3: make the map interactive
+// MapEvents enables the event system
+// Behavior implements default interactions for pan/zoom (also on mobile touch environments)
+var behavior = new H.mapevents.Behavior(new H.mapevents.MapEvents(map));
 
-  // Create the default UI:
-  var ui = H.ui.UI.createDefault(map, defaultLayers);   
+// Step 4: Create the default UI:
+var ui = H.ui.UI.createDefault(map, defaultLayers, 'en-US');
 
-
-
-
-
-
+// Add the click event listener.
+addDraggableMarker(map, behavior);
 
 
-  
+
+
+
+
